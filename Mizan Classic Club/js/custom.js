9b115c1fffc0afa1c41f4dffbf2b9d3fe9ead66f
Shakib Hosen Sabbir <65281890+shakib-hosen@users.noreply.github.com>
2022-02-26 18:43:23 +0600

    Add files via upload

---
+++ Mizan Classic Club/js/custom.js
@@ -0,0 +1,38 @@
+
+var typed = new Typed(".type",{
+    strings:["Front End Web designer", "Youtuber", "Motivational Speaker"],
+        typeSpeed:100,
+            backspeed:100,
+                loop:true,
+});
+
+// Mixitup
+var mixer = mixitup('.filter');
+
+
+
+
+
+// navbar click hide Collapse
+
+$(document).ready(function () { 
+    $(document).click(function () {
+       // if($(".navbar-collapse").hasClass("in")){
+         $('.navbar-collapse').collapse('hide');
+       // }
+    });
+  });
+
+
+  // New Code Start
+
+
+
+
+
+
+
+
+
+
+
